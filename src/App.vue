fe7a116a4f929f2d76477ec59a3bd85006a2c57d
jackdizhu <376365334@qq.com>
2017-12-09 19:02:44 +0800

    声明周期

--- src/App.vue
+++ src/App.vue
@@ -21,9 +21,21 @@ export default {
   watch: {},
   // 事件方法
   methods: {},
-  // 创建完毕
+  // el 和 data 并未初始化
+  beforeCreate: () => {
+    console.log('beforeCreate ------------- el 和 data 并未初始化')
+  },
+  // 完成了 data 数据的初始化，el没有
   created: () => {
-    console.log('created')
+    console.log('created ------------------ 完成了 data 数据的初始化，el没有')
+  },
+  // 完成了 el 和 data 初始化
+  beforeMount: () => {
+    console.log('beforeMount -------------- 完成了 el 和 data 初始化')
+  },
+  // 完成挂载
+  mounted: () => {
+    console.log('mounted ------------------ 完成挂载')
   }
 }
 </script>
